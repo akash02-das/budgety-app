d6276d4b510e9c4948c63f71438d61ed11f34b53
Akash <aku.das02@gmail.com>
2020-08-31 21:03:41 +0600

    Income and expense function constructors

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // BUDGET CONTROLLER
 const budgetController = (function () {
 
-    // Some Code
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
 })();
 
